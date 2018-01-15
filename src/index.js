cf9eaf8df7dc7916c7f421a10ac5698efc9b51ca
Nickrosendo <nicolasrosendo@hotmail.com>
2018-01-14 22:12:10 -0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import './css/login.css';
-import './css/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login';
-import Home from './components/Home';
-import registerServiceWorker from './registerServiceWorker';
-import { Router, Route, browserHistory } from 'react-router';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/login" component={Login}></Route>
-    <Route path="/" component={Home}></Route>
-  </Router>
-),
-  document.getElementById('root')
-);
+import './css/Reset.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from "./App.js";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App/>, document.getElementById("root"));
 registerServiceWorker();
