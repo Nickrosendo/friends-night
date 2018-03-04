da9afa8483ccc10205c111a101fb9fc52fb015f8
Nickrosendo <nicolasrosendo@hotmail.com>
2018-03-04 17:16:49 -0300

    heroku api

--- src/app/services/http.js
+++ src/app/services/http.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
 
-export const createClient = (options = {}) => axios.create(Object.assign({ baseURL: "http://localhost:8080" }, options))
+export const createClient = (options = {}) => axios.create(Object.assign({ baseURL: "https://api-friends-night.herokuapp.com" }, options))
 
 export default createClient()
